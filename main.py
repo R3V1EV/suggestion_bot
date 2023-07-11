1e57d9a10cf30873dec4264b7b96f87caa11e691
R3V1EV <35421934+R3V1EV@users.noreply.github.com>
2023-07-11 18:49:13 +0300

    main file of bot

---
+++ main.py
@@ -0,0 +1,66 @@
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+# Установите токен вашего бота
+TOKEN = 'TOKEN'
+
+# Установите ID вашего админского канала
+ADMIN_CHANNEL_ID = 'CHAT_ID'
+
+# Установите ID канала, в который нужно отправить фото
+PUBLISHED_CHANNEL_ID = 'CHANNEL_ID'
+
+# Инициализация бота и диспетчера
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message_handler(content_types=types.ContentTypes.PHOTO)
+async def handle_photo(message: types.Message):
+    photo = message.photo[-1]  # Получаем последнюю (наибольшую) фотографию из списка
+    caption = f'Пост от {message.from_user.full_name}'
+
+    # Создаем клавиатуру с кнопками "Опубликовать" и "Отменить"
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    publish_button = InlineKeyboardButton('Опубликовать', callback_data='publish')
+    cancel_button = InlineKeyboardButton('Отменить', callback_data='cancel')
+    keyboard.add(publish_button, cancel_button)
+
+    # Отправляем фото в админский канал с клавиатурой
+    await bot.send_photo(chat_id=ADMIN_CHANNEL_ID, photo=photo.file_id, caption=caption, reply_markup=keyboard)
+
+
+@dp.callback_query_handler(lambda callback_query: True)
+async def handle_callback_query(callback_query: types.CallbackQuery):
+    if callback_query.data == 'publish':
+        # Ответ на нажатие кнопки "Опубликовать"
+        await bot.send_message(chat_id=callback_query.from_user.id, text='пост опубликован в канале.')
+        await bot.edit_message_reply_markup(callback_query.from_user.id, callback_query.message.message_id)
+        # Отправляем фото в целевой канал
+        photo_message = callback_query.message.reply_to_message
+        photo = callback_query.message.photo[-1]
+        caption = callback_query.message.caption if hasattr(callback_query.message, 'caption') else None
+        await bot.send_photo(chat_id=PUBLISHED_CHANNEL_ID, photo=photo.file_id, caption=caption)
+
+    elif callback_query.data == 'cancel':
+        # Ответ на нажатие кнопки "Отменить"
+        await bot.send_message(chat_id=callback_query.from_user.id, text='Отмена.')
+        await bot.edit_message_reply_markup(callback_query.from_user.id, callback_query.message.message_id)
+
+# Запуск бота
+async def main():
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+
+if __name__ == '__main__':
+    # Запуск бота
+    logging.info('Starting bot...')
+    asyncio.run(main())
